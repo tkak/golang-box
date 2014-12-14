a70f19268a2d4dd2d023e41bcb13badd6709e612
tkak <takaaki.frkw@gmail.com>
2014-12-14 15:54:07 +0900

    modify peco function

--- packer/scripts/centos/golang.sh
+++ packer/scripts/centos/golang.sh
@@ -5,25 +5,20 @@ install_devtools(){
   yum install -y vim-enhanced git hg zsh
   touch /home/vagrant/.zshrc
   wget --no-check-certificate http://install.ohmyz.sh -O - | sudo -i -u vagrant sh
+
+  ## vimrc
   curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sudo -i -u vagrant sh
   git clone https://gist.github.com/3ab1720d332700cbf6e0.git /tmp/golang-vimrc
   sudo -i -u vagrant cp /tmp/golang-vimrc/.vimrc /home/vagrant/
+
+  ## GOPATH, ghq, peco
   echo 'export GOPATH=$HOME' >> /home/vagrant/.zshrc
   echo 'export PATH=$PATH:$GOPATH/bin' >> /home/vagrant/.zshrc
   sudo -i -u vagrant zsh -c 'source ~/.zshrc; go get github.com/motemen/ghq'
   sudo -i -u vagrant zsh -c 'source ~/.zshrc; go get github.com/peco/peco/cmd/peco'
-  cat <<EOT >> /home/vagrant/.zshrc
-function peco-src () {
-    local selected_dir=$(ghq list --full-path | peco --query "$LBUFFER")
-    if [ -n "$selected_dir" ]; then
-        BUFFER="cd ${selected_dir}"
-        zle accept-line
-    fi
-    zle clear-screen
-}
-zle -N peco-src
-bindkey '^]' peco-src
-EOT
+  git clone https://gist.github.com/9d00b8adb339880c432f.git /tmp/zsh-peco
+  sudo -i -u vagrant cat /tmp/zsh-peco/.zshrc.peco >> /home/vagrant/.zshrc
+  sudo -i -u vagrant zsh -c 'git config --global ghq.root ~/src'
 }
 
 install_golang(){
